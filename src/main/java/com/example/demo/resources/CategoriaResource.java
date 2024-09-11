886593a34c44e46bfcaa2acebeff68904273964a
Betinsk <gmdust@hotmail.com>
2024-09-11 16:14:14 -0300

    go ahead

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -1,16 +1,36 @@
 package com.example.demo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Categoria;
+import com.example.demo.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categoria")
 public class CategoriaResource {
 	
-	@GetMapping
-	public String listar() {
-		return "Rest está funcionando";
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		
+			Categoria  obj = categoriaService.find(id);
+		
+		/*	
+			List<Categoria> list = new ArrayList<>();
+			list.add(cat1);
+			list.add(cat2); */
+			
+		return ResponseEntity.ok().body(obj);
 	}
 		
 		
